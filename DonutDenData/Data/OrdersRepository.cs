2eebe5a5079098f81e29f3cc4af1df4cffebe81f
Tim Harley <Timothy.D.Harley@gmail.com>
2019-07-20 13:51:21 -0500

    woring on Orders Repository

---
+++ DonutDenData/Data/OrdersRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data.SqlClient;
+using DonutDenData.Models;
+
+namespace DonutDenData.Data
+{
+    public class OrdersRepository
+    {
+        const string ConnectionString = "Server=localhost; Database=PartingPets; Trusted_Connection=True;";
+
+        public Orders AddOrder(CreateOrderRequest newOrderObj)
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var newOrderQuery = db.QueryFirstOrDefault<Orders>(@"
+                    Insert into Orders (firstName, lastName, email, phoneNumber, pickupDate, pickupTime, isApproved, approvedBy, isDeleted)
+                    Output inserted.*
+                    Values()")
+            }
+        }
+    }
+}
