328dc71bd6c12d895f3f6cdebbbbcbc3ecf82358
Tim Harley <Timothy.D.Harley@gmail.com>
2019-07-21 04:41:15 -0500

    wip

--- DonutDenData/Data/OrdersRepository.cs
+++ DonutDenData/Data/OrdersRepository.cs
@@ -19,7 +19,33 @@ namespace DonutDenData.Data
                 var newOrderQuery = db.QueryFirstOrDefault<Orders>(@"
                     Insert into Orders (firstName, lastName, email, phoneNumber, pickupDate, pickupTime, isApproved, approvedBy, isDeleted)
                     Output inserted.*
-                    Values()")
+                    Values(@firstName,@lastName,@email,@phoneNumber,@pickupDate,@pickupTime,@isApproved,@approvedBy,0)",
+                    new
+                    {
+                        newOrderObj.FirstName,
+                        newOrderObj.LastName,
+                        newOrderObj.Email,
+                        newOrderObj.PhoneNumber,
+                        newOrderObj.PickupDate,
+                        newOrderObj.PickupTime,
+                        newOrderObj.IsApproved,
+                        newOrderObj.ApprovedBy,
+                        newOrderObj.IsDeleted
+                    });
+                if (newOrderQuery != null)
+                {
+                    return newOrderQuery;
+                }
+
+                throw new Exception("No Order Found");
+            }
+        }
+
+        public IEnumerable<Orders> GetOrdersByDate(DateTime date)
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var orderByDateQuery = "select * from Orders where Orders"
             }
         }
     }
