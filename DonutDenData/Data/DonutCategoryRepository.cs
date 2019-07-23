6b34797240f222342cd8d0b836e3334519689f41
Tim Harley <Timothy.D.Harley@gmail.com>
2019-07-23 18:51:10 -0500

    added DonutCategoryRepository

---
+++ DonutDenData/Data/DonutCategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Dapper;
+using DonutDenData.Models;
+
+namespace DonutDenData.Data
+{
+    public class DonutCategoryRepository
+    {
+        const string ConnectionString = "Server=localhost; Database=PartingPets; Trusted_Connection=True;";
+
+        public IEnumerable<DonutCategory> GetAllDonutCategories()
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                return db.Query<DonutCategory>("select * from donutCategory");
+            }
+        }
+
+        public DonutCategory GetDonutCategoryById(int id)
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                return db.QueryFirstOrDefault<DonutCategory>("select * from donutCategory where id = @id", new { id });
+            }
+        }
+    }
+}
