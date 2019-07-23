fa147431b5d677739469b8274f90bdf3be1bcf25
Tim Harley <Timothy.D.Harley@gmail.com>
2019-07-22 19:34:08 -0500

    added CreateOrderRequest file

---
+++ DonutDenData/Models/CreateOrderRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DonutDenData.Models
+{
+    public class CreateOrderRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime PickupDate { get; set; }
+        public DateTime PickupTime { get; set; }
+        public bool IsApproved { get; set; }
+        public int ApprovedBy { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
